57f4de63892a254efba2923f6f4c68789e3ae6ff
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-01-31 23:20:56 -0800

    adding Inter font

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         </h1>
       </a>
       <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-        A minimal plain text storage service
+        A minimal plain text storage service.
       </p>
     </header>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,15 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme.js";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 } satisfies Config;
