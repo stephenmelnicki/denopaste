472c61f1489b1ae6e504bfd6c64f05bc9ef1ca39
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-02-06 11:13:55 -0800

    adding new favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -14,6 +14,11 @@ export default function App({ Component }: PageProps) {
           name="description"
           content="A plain text storage service for Deno 🦕"
         />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%221em%22 font-size=%2280%22>🦕</text></svg>"
+        >
+        </link>
         <link rel="stylesheet" type="text/css" href="/styles.css" />
       </head>
       <body class="px-4 py-8 mx-auto max-w-screen-md">
