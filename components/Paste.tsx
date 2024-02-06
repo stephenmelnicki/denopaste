4c808bc9bd673194a3774bc310aae9ad476b3214
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-02-06 10:54:49 -0800

    fixing line's right border color

--- components/Paste.tsx
+++ components/Paste.tsx
@@ -51,7 +51,8 @@ function Line({ line, index }: LineProps) {
     "before:text-right",
     "before:content-[attr(data-line-number)]",
     "before:border-r",
-    "before:border-gray-600",
+    "before:border-gray-300",
+    "before:dark:border-gray-500",
   ];
 
   return (
