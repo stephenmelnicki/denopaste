dd606c583d6002eae642dbe7691da2a25f215d09
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-03-21 11:31:14 -0700

    fixing font loading and some other fixes

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -18,6 +18,13 @@ export default function App({ Component }: PageProps) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%221em%22 font-size=%2280%22>🦕</text></svg>"
         >
         </link>
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/InterVariable.woff2"
+          type="font/woff2"
+          crossorigin="anonymous"
+        />
         <link rel="stylesheet" type="text/css" href="/styles.css" />
       </head>
       <body class="px-4 py-8 mx-auto max-w-screen-md">
