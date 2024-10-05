fe52ebc3e44313cd8a93612cd86d3c914b2fcb9e
Stephen Melnicki <hello@stephenmelnicki.com>
2024-10-05 10:18:09 -0700

    removing old font face declarations and moving social preview header

--- middlewares/reporter_test.ts
+++ middlewares/reporter_test.ts
@@ -4,8 +4,8 @@ import reporter from "./reporter.ts";
 import type { FreshContext } from "fresh";
 
 Deno.test("reporter should warn if no hostname or access token has been provided", () => {
-  const warn = stub(console, "warn");
-  const _ = stub(Deno.env, "get", () => undefined);
+  using _ = stub(Deno.env, "get", () => undefined);
+  using warn = stub(console, "warn");
 
   const middleware = reporter();
   middleware({
